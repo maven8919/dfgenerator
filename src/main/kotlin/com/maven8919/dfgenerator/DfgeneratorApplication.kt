78ca7ae492ed68bdbb20b509a37e6e91aafc6ea6
maven8919 <maven8919@gmail.com>
2017-08-04 17:39:36 +0200

    Initial commit

---
+++ src/main/kotlin/com/maven8919/dfgenerator/DfgeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.maven8919.dfgenerator
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DfgeneratorApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(DfgeneratorApplication::class.java, *args)
+}
