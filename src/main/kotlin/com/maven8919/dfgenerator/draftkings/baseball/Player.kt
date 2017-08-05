19335cf58dc70b893df721e92fec7f13690078dc
maven8919 <maven8919@gmail.com>
2017-08-05 12:06:13 +0200

    Start implementing lineup generator script

---
+++ src/main/kotlin/com/maven8919/dfgenerator/draftkings/baseball/Player.kt
@@ -0,0 +1,12 @@
+package com.maven8919.dfgenerator.draftkings.baseball
+
+class Player {
+
+    var position : String = ""
+    var name : String = ""
+    var salary : Int = Int.MAX_VALUE
+    var gameInfo : String = ""
+    var avgPointsPerGame : Double = 0.0
+    var teamAbbrev : String = ""
+
+}
--- src/main/kotlin/com/maven8919/dfgenerator/draftkings/baseball/controller/DfBaseballController.kt
+++ src/main/kotlin/com/maven8919/dfgenerator/draftkings/baseball/controller/DfBaseballController.kt
@@ -1,13 +1,30 @@
 package com.maven8919.dfgenerator.draftkings.baseball.controller
 
+import com.maven8919.dfgenerator.draftkings.baseball.Player
+import com.maven8919.dfgenerator.draftkings.baseball.service.PlayerService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.multipart.MultipartFile
 
 @Controller
 class DfBaseballController {
 
-    @GetMapping("df_baseball")
-    fun dfBaseball() : String {
+    @Autowired lateinit var playerService : PlayerService
+
+    @ModelAttribute("players")
+    fun players(@RequestParam("playersCsv") file : MultipartFile) {
+        var players = playerService.getPlayers(file)
+        var starters = playerService.generateStarters(players)
+        return starters
+
+    }
+
+    @PostMapping("df_baseball")
+    fun dfBaseball(@RequestParam("playersCsv") file : MultipartFile, model : Model) : String {
         return ""
     }
 
